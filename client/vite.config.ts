40c7c563e4365063c6993137504d5608b4ae420c
Matthew Ivanov <matthewiva13@gmail.com>
2023-07-06 11:23:01 +0300

    gcloud

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8080',
+        target: 'https://email-verifier-392008.lm.r.appspot.com',
         changeOrigin: true,
         secure: false,
       },
